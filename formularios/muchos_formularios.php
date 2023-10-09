45f1ffe4a36046c5d5dd39147aae269065bea4c1
Ferdomi21 <Ferdomi21>
2023-10-09 14:28:12 +0200

    Subiendo tercera funcion

--- formularios/muchos_formularios.php
+++ formularios/muchos_formularios.php
@@ -106,5 +106,18 @@ function numMayor(int $num1, int $num2, int $num3): string
         return "El mayor es $candidato";
     }
 }
+function potencia(int $base, int $exponente): int
+{
+    $resultado = 1;
+
+    if ($exponente >= 0) {
+
+        for ($i = 1; $i <= $exponente; $i++) {
+            $resultado *= $base; # $resultado = $resultado * $base
+        }
+        return $resultado;
+    }
+    return 0;
+}
 
 ?>
